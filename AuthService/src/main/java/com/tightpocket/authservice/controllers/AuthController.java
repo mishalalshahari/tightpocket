6d543bb1595cf1de357f2045d889ea64efb30a1d
Mishal Al Shahari <mishalalshahari@gmail.com>
2024-10-27 23:52:22 +0530

    auth/ created configs and controllers

---
+++ AuthService/src/main/java/com/tightpocket/authservice/controllers/AuthController.java
@@ -0,0 +1,50 @@
+package com.tightpocket.authservice.controllers;
+
+import com.tightpocket.authservice.entities.RefreshToken;
+import com.tightpocket.authservice.models.UserInfoDTO;
+import com.tightpocket.authservice.responses.JwtResponseDTO;
+import com.tightpocket.authservice.services.JwtService;
+import com.tightpocket.authservice.services.RefreshTokenService;
+import com.tightpocket.authservice.services.implementation.UserDetailsServiceImpl;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+public class AuthController {
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private RefreshTokenService refreshTokenService;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsServiceImpl;
+
+    @PostMapping("/auth/v1/register")
+    public ResponseEntity register(@RequestBody UserInfoDTO userInfoDTO) {
+        try {
+            Boolean isRegistered = userDetailsServiceImpl.registerUser(userInfoDTO);
+            if(Boolean.FALSE.equals(isRegistered)) {
+                return new ResponseEntity<>("Already exists!", HttpStatus.BAD_REQUEST);
+            }
+            RefreshToken refreshToken = refreshTokenService.createRefreshToken(userInfoDTO.getUsername());
+            String jwtToken = jwtService.GenerateToken(userInfoDTO.getUsername());
+            return new ResponseEntity<>(JwtResponseDTO
+                    .builder()
+                    .accessToken(jwtToken)
+                    .token(refreshToken
+                            .getToken()
+                    )
+                    .build(), HttpStatus.OK);
+        } catch (Exception ex) {
+            return new ResponseEntity<>("Exception in User Service", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
--- AuthService/src/main/java/com/tightpocket/authservice/services/JwtService.java
+++ AuthService/src/main/java/com/tightpocket/authservice/services/JwtService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -39,6 +40,11 @@ public class JwtService {
     public Boolean validateToken(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    public String GenerateToken(String username){
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, username);
     }
 
     public String createToken(Map<String, Object> claims, String username) {
