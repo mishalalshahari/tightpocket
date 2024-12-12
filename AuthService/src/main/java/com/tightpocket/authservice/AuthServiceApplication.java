727d5d27d7bdaf4fcc33b4b2012e9c2d091c5fd8
Mishal Al Shahari <mishalalshahari@gmail.com>
2024-12-12 21:58:18 +0530

    added userservice

--- AuthService/src/main/java/com/tightpocket/authservice/AuthServiceApplication.java
+++ AuthService/src/main/java/com/tightpocket/authservice/AuthServiceApplication.java
@@ -1,5 +1,6 @@
 package com.tightpocket.authservice;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("MYSQL_PASSWORD", dotenv.get("MYSQL_PASSWORD"));
+		System.setProperty("STATIC_IP", dotenv.get("STATIC_IP"));
 		SpringApplication.run(AuthServiceApplication.class, args);
 	}
-
 }
